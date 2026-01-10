91ce3f4e1d0eafb2a0274f934af34cbc5bb1ebae
Pranav Raj KPV <kpvpranavraj2305@gmail.com>
2026-01-10 13:12:33 +0530

    App page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [page, setPage] = useState("login"); // login | signup
+
+  if (isLoggedIn) {
+    return <Home setIsLoggedIn={setIsLoggedIn} />;
+  }
+
   return (
-    <>
-      <p>Haiii</p>
-    </>
-  )
+    <div style={styles.container}>
+      <h2>Welcome 👋</h2>
+
+      <div style={styles.buttonBox}>
+        <button onClick={() => setPage("login")} style={styles.button}>
+          Login
+        </button>
+        <button onClick={() => setPage("signup")} style={styles.button}>
+          Signup
+        </button>
+      </div>
+
+      {page === "login" && <Login setIsLoggedIn={setIsLoggedIn} />}
+      {page === "signup" && <Signup />}
+    </div>
+  );
 }
 
-export default App
+export default App;
